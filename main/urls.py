4b952cfaae1b2f7636ff3154d90f15414f3ac638
revanthkolli <revanthkolli@gmail.com>
2015-09-05 05:09:59 -0400

    Updated UI look and created captions page, can now get image from url

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('main.views',
     url(r'^$', 'home', name='home'),
     url(r'^imageUpload/$', 'imageUpload', name='imageUpload'),
+    url(r'^urlUpload/$', 'urlUpload', name='urlUpload'),
 )
--- static/AutoCaption/js/app.js
+++ static/AutoCaption/js/app.js
@@ -3,5 +3,12 @@ Dropzone.options.myAwesomeDropzone = {
   paramName: "file", // The name that will be used to transfer the file
   maxFilesize: 2, // MB
   clickable: true,
+  init: function () {
+   this.on("complete", function (file) {
+     if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+       window.location.href = {% url 'main:imageUpload' %};
+     }
+   });
+ }
 };
 var myDropzone = new Dropzone("#my-awesome-dropzone", Dropzone.options.myAwesomeDropzone);
