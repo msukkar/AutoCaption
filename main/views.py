1d5b791501869a806889f84b6d81abf52dac4259
revanthkolli <revanthkolli@gmail.com>
2015-09-05 01:57:14 -0400

    Finished file upload, saves file to server

--- main/views.py
+++ main/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect, render_to_response
+from django.views.decorators.csrf import csrf_protect
 
 def home(request):
     return render(request, 'index.html')
+
+@csrf_protect
+def imageUpload(request):
+    print("Hi")
+    if request.method == 'POST':
+        file = request.FILES['file']
+        filename = "media/" + str(file.name)
+        with open(filename, 'wb') as dest:
+            for chunk in file.chunks():
+                dest.write(chunk)
+    return redirect('main:home')
--- static/AutoCaption/js/app.js
+++ static/AutoCaption/js/app.js
@@ -1,12 +1,7 @@
-var ready;
-
-ready = function() {
-
-
-}
-
-var myDropzone = new Dropzone("#my-awesome-dropzone", { url: "/file/post"});
-
-$(document).ready(ready);
-
-$(document).on('page:load', ready);
+Dropzone.autoDiscover = false;
+Dropzone.options.myAwesomeDropzone = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 2, // MB
+  clickable: true,
+};
+var myDropzone = new Dropzone("#my-awesome-dropzone", Dropzone.options.myAwesomeDropzone);
