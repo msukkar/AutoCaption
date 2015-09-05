f8cde53024d92f2c191d6b1d81c1402c28a3631a
revanthkolli <revanthkolli@gmail.com>
2015-09-05 11:57:31 -0400

    Removed Dropzone and built custom drag/drop, made upload and external url work the same way

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'home', name='home'),
     url(r'^imageUpload/$', 'imageUpload', name='imageUpload'),
     url(r'^urlUpload/$', 'urlUpload', name='urlUpload'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect, render_to_response
 from django.views.decorators.csrf import csrf_protect
+from django.conf import settings
 
 def home(request):
     return render(request, 'index.html')
@@ -8,10 +9,11 @@ def home(request):
 def imageUpload(request):
     if request.method == 'POST':
         file = request.FILES['file']
-        filename = "static/AutoCaption/css/media/" + file.name
+        filename = str("static/AutoCaption/css/media/" + file.name)
         with open(filename, 'wb') as dest:
             for chunk in file.chunks():
                 dest.write(chunk)
+        filename = "/" + filename;
         return render(request, 'captions.html', {'image': filename})
     return redirect('main:home')
 
