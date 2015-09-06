310e69b18fcb9426fefb4514c94da1a6ee99ca02
Kienan Adams <kra8ff@virginia.edu>
2015-09-05 20:51:39 -0400

    Add loading animation after file upload

--- static/AutoCaption/js/app.js
+++ static/AutoCaption/js/app.js
@@ -1,7 +1,40 @@
-Dropzone.autoDiscover = false;
-Dropzone.options.myAwesomeDropzone = {
-  paramName: "file", // The name that will be used to transfer the file
-  maxFilesize: 2, // MB
-  clickable: true
-};
-var myDropzone = new Dropzone("#my-awesome-dropzone", Dropzone.options.myAwesomeDropzone);
+// Dropzone.autoDiscover = false;
+// Dropzone.options.myAwesomeDropzone = {
+//   paramName: "file", // The name that will be used to transfer the file
+//   maxFilesize: 2, // MB
+//   clickable: true
+// };
+// var myDropzone = new Dropzone("#my-awesome-dropzone", Dropzone.options.myAwesomeDropzone);
+
+var ready;
+
+ready = function() {
+
+	$('#url').keypress(function(e) {
+		var code = (e.keyCode ? e.keyCode : e.which);
+		if (code == 13) {
+			$('.container').addClass('hidden');
+        	$('.loading').removeClass('hidden');
+		}
+	});
+
+
+	$('.fileUpload').on('change', function (e) {
+		$('.container').addClass('hidden');
+        $('.loading').removeClass('hidden');
+	});
+
+}
+
+// function keypressInBox(e) {
+//     var code = (e.keyCode ? e.keyCode : e.which);
+//     if (code == 13) { //Enter keycode
+//         e.preventDefault();
+//         // $('.container').addClass('hidden');
+//         // $('.loading').removeClass('hidden');
+//     }
+// };
+
+$(document).ready(ready);
+
+$(document).on('page:load', ready);
